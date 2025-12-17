87b612ed378c6361996a735de87828913782666e
Aaron Tanguma <the_guma@yahoo.com>
2025-12-16 17:15:30 -0700

    created method Add(), that adds two integers and returns the sum

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,14 @@
 {
     public class Program
     {
+        public static int Add(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(3, 6));
         }
     }
 }
