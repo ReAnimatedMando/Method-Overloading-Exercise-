baf86bf93703a260109ee881a6b79e468d3b16a5
Aaron Tanguma <the_guma@yahoo.com>
2025-12-16 17:20:12 -0700

    created an overload method, but with decimals as the return type and parameters

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,14 +2,22 @@
 {
     public class Program
     {
-        public static int Add(int numberOne, int numberTwo)
+        public static int Add(int numberOne, int numberTwo) // create method to return the sum of two integers
         {
-            return numberOne + numberTwo;
+            return numberOne + numberTwo; // return the sum of parameters
         }
+
+        public static decimal Add(decimal numberOne, decimal numberTwo) // create method to return the sum of two decimals
+        {
+            return numberOne + numberTwo; // return the sum of the parameters
+        }
+        
         
         static void Main(string[] args)
         {
-            Console.WriteLine(Add(3, 6));
+            Console.WriteLine(Add(3, 6)); // call Add() method using integers
+            Console.WriteLine(Add(30.46m, 4.02m)); // call Add() method using decimals
+            Console.WritLine(Add());
         }
     }
 }
